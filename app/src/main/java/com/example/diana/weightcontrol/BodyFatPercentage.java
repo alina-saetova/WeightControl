2e677e190319d325e9a9262eb88f9385b781d329
TaliyaR <taliya200499@gmail.com>
2019-04-20 02:23:25 +0300

    new_bfp

--- app/src/main/java/com/example/diana/weightcontrol/BodyFatPercentage.java
+++ app/src/main/java/com/example/diana/weightcontrol/BodyFatPercentage.java
@@ -1,14 +1,24 @@
 package com.example.diana.weightcontrol;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import static com.example.diana.weightcontrol.R.id.cancel_action;
+import static com.example.diana.weightcontrol.R.id.radio_but_female;
+import static com.example.diana.weightcontrol.R.id.radio_but_male;
+import static com.example.diana.weightcontrol.R.id.visible;
 
 
 public class BodyFatPercentage extends Fragment implements View.OnClickListener {
@@ -16,8 +26,10 @@ public class BodyFatPercentage extends Fragment implements View.OnClickListener 
     double heightInt, waistInt, hipInt, neckInt;
     double percent;
     Button button;
-    TextView result;
+    TextView result, row1, row2, row3, row4;
     RadioGroup radioGroup;
+    RadioButton radioBut1, radioBut2;
+    TableLayout table;
 
 
     @Override
@@ -30,45 +42,34 @@ public class BodyFatPercentage extends Fragment implements View.OnClickListener 
         waist = view.findViewById(R.id.editText_waist);
         hip = view.findViewById(R.id.editText_hip);
         neck = view.findViewById(R.id.editText_neck);
-//
+        row1 = view.findViewById(R.id.row1);
+        row2 = view.findViewById(R.id.row2);
+        row3 = view.findViewById(R.id.row3);
+        row4 = view.findViewById(R.id.row4);
+
         result = view.findViewById(R.id.text_result);
         radioGroup = view.findViewById(R.id.radio_group);
 
-        button = view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculateVolume();
-                result.setText("Результат " + String.valueOf(percent));
-            }
-        });
+        radioBut1 = view.findViewById(R.id.radio_but_female);
+        radioBut2 = view.findViewById(R.id.radio_but_male);
 
+        table = view.findViewById(R.id.table);
+
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(this);
         return view;
     }
 
-    public void calculateVolume() {
-        heightInt = Double.valueOf(height.getText().toString());
-        waistInt = Double.valueOf(waist.getText().toString());
-        hipInt = Double.valueOf(hip.getText().toString());
-        neckInt = Double.valueOf(neck.getText().toString());
-
-        int idRadioBut = radioGroup.getCheckedRadioButtonId();
-
-        if (idRadioBut == R.id.radio_but_female) {
-            percent = percentFatFemale(heightInt, waistInt, hipInt, neckInt);
-        } else if (idRadioBut == R.id.radio_but_male) {
-            percent = percentFatMale(heightInt, waistInt, neckInt);
-        }
-
-    }
 
     public static double percentFatMale(double height, double waist, double neck) {
         double percent = 495 / (1.0324 - (0.19077 * (Math.log10(waist - neck))) + (0.15456 * (Math.log10(height)))) - 450;
+        percent = (double)Math.round(percent * 100) / 100;
         return percent;
     }
 
     public static double percentFatFemale(double height, double waist, double hip, double neck) {
         double percent = 495 / (1.29579 - 0.35004 * (Math.log10(waist + hip - neck)) + 0.22100 * (Math.log10(height))) - 450;
+        percent = (double) Math.round(percent * 100) / 100;
         return percent;
     }
 
@@ -76,5 +77,67 @@ public class BodyFatPercentage extends Fragment implements View.OnClickListener 
     @Override
     public void onClick(View v) {
 
+        String heightS = height.getText().toString();
+        String waistS = waist.getText().toString();
+        String hipS = hip.getText().toString();
+        String neckS = neck.getText().toString();
+
+        int idGen = radioGroup.getCheckedRadioButtonId();
+
+        if(!heightS.isEmpty() && !waistS.isEmpty() && !hipS.isEmpty() && !neckS.isEmpty()){
+            switch (idGen){
+                case R.id.radio_but_female:
+                    row1.setText("< 22%");
+                    row2.setText("23%-34%");
+                    row3.setText("35%-40%");
+                    row4.setText("> 40%");
+                    table.setVisibility(View.VISIBLE);
+                    percent = percentFatFemale(Integer.parseInt(heightS), Integer.parseInt(waistS),
+                            Integer.parseInt(hipS), Integer.parseInt(neckS));
+                    result.setText("Процент жира в организме: " + String.valueOf(percent) + "%");
+                    if (percent <= 22.0) {
+                        row1.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    if ((percent >= 23.0) & (percent <= 34.0)) {
+                        row2.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    if ((percent >= 35.0) && (percent <= 40.0)) {
+                        row3.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    if (percent > 40.0) {
+                        row4.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    result.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.radio_but_male:
+                    row1.setText("< 8%");
+                    row2.setText("9%-20%");
+                    row3.setText("21%-25%");
+                    row4.setText("> 25%");
+                    table.setVisibility(View.VISIBLE);
+                    percent = percentFatMale(Integer.parseInt(heightS), Integer.parseInt(waistS),
+                            Integer.parseInt(neckS));
+                    result.setText("Процент жира в организме: " + String.valueOf(percent) + "%");
+                    if (percent <= 8.0) {
+                        row1.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    if ((percent >= 9.0) & (percent <= 20.0)) {
+                        row2.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    if ((percent >= 21.0) && (percent <= 25.0)) {
+                        row3.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    if (percent > 25.0) {
+                        row4.setTextColor(getResources().getColor(R.color.colorAccent));
+                    }
+                    result.setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+        else {
+            result.setText("Проверьте введенные данные и повторите попытку");
+            result.setTextColor(Color.RED);
+            result.setVisibility(View.VISIBLE);
+        }
     }
 }
